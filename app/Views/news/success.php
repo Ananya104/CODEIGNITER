039355d1a60d7e24c30204dc3c2a7cfe5108bfb9
Ananya104 <ananyam1702@gmail.com>
2022-10-22 13:40:18 -0400

    added all files.

---
+++ app/Views/news/success.php
@@ -0,0 +1,3 @@
+<body bgcolor="powderblue">
+<h1 style="color:blue; font-family:times new roman">News item created successfully!</h1>
+</body>
---
+++ app/Views/templates/header.php
@@ -0,0 +1,8 @@
+<!doctype html>
+<html>
+<head>
+<title>Codeigniter-1</title>
+</head>
+<body>
+<h1><?=esc($title)?></h1>
+
